4ce48be5193867e2ea44b6b990a801a2f330fab0
dsuschinsky <dsuschinsky@gmaiil.com>
2017-11-13 23:17:59 +0300

    Add timezone, locale, version app, about.html, author.html, readme.md

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require_relative 'initializers/version'
 
 require 'rails/all'
 
@@ -10,6 +11,9 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Europe/Minsk'
+
+    config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
