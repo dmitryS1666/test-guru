83fea9ecfdfa02e59db82c0b4c8f2548aead9030
Dmitry <dsuschinsky@gmail.com>
2017-11-17 15:45:07 +0300

    update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def test_level(level)
+    Test.where('level = ?', level)
+  end
 end
---
+++ db/migrate/20171117074618_add_questions_test_id.rb
@@ -0,0 +1,5 @@
+class AddQuestionsTestId < ActiveRecord::Migration[5.1]
+  def change
+    add_column(:questions, :test_id, :integer)
+  end
+end
