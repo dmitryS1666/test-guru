9c3091e9c0403525e64410acadb8a1caf91c7620
Dmitry <dsuschinsky@gmail.com>
2017-11-20 17:04:12 +0300

    update models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
 
   def self.tests_by_category(category)
-    Test.joins('INNER JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order('tests.created_at DESC')
+    Test.select('title').joins('INNER JOIN categories ON tests.category_id = categories.id').where('categories.title = ?', category).order('tests.created_at DESC')
   end
 
 end
