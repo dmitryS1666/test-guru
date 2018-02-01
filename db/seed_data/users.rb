48b3b7247de9f5566ccc999e4db731658ca5abb8
Dmitry <dsuschinsky@gmail.com>
2018-02-01 23:37:23 +0300

    update master

--- db/seed_data/users.rb
+++ db/seed_data/users.rb
@@ -1,8 +1,8 @@
 10.times { User.create!(email: Faker::Internet.email, password: Faker::Internet.password) }
 user_admin = [
-    ['admin@admin.com', 123123]
+    %w(admin@admin.com 123123 Admin)
 ]
 
-user_admin.each do |email, password|
-  User.create!(email: email, password: password)
+user_admin.each do |email, password, type|
+  User.create!(email: email, password: password, type: type)
 end
