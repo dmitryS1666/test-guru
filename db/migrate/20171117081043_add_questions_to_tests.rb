4447dcbe08c291b6b42a86f11d0b4f9fb01849c6
Dmitry <dsuschinsky@gmail.com>
2018-02-01 23:35:10 +0300

    update master

--- db/migrate/20171117081043_add_questions_to_tests.rb
+++
@@ -1,6 +0,0 @@
-class AddQuestionsToTests < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :questions, :test_id, foreign_key: true
-    add_foreign_key :questions, :tests
-  end
-end
--- db/seed_data/users.rb
+++ db/seed_data/users.rb
@@ -1,1 +1,8 @@
 10.times { User.create!(email: Faker::Internet.email, password: Faker::Internet.password) }
+user_admin = [
+    ['admin@admin.com', 123123]
+]
+
+user_admin.each do |email, password|
+  User.create!(email: email, password: password)
+end
